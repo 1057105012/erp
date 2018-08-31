29888ebff1a1c4192bb8cf7fc1fadb6136cd64c7
mldn <mldnqu@163.com>
2018-08-31 23:48:14 +0800

    -a

--- Hello.java
+++ Hello.java
@@ -2,5 +2,11 @@ class Main
 {
 	public static void main(String[] args){
 		System.out.println("Hello");
+				System.out.println("Hello");
+
+		System.out.println("Hello");
+				System.out.println("Hello");
+						System.out.println("Hello");
+
 	}
 }
